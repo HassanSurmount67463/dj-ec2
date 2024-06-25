f0a159c038e4b30b10ce224fce6a7602c27d6b59
HassanSurmount67463 <hassan67463@gmail.com>
2024-06-25 14:20:06 +0500

    create NFT model

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,7 @@
-# from django.contrib import admin
+from django.contrib import admin
+from .models import NFT
 
-# Register your models here.
+
+@admin.register(NFT)
+class NFTAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "price", "created_at", "updated_at"]
