43df04a18dd012450fd4d917f9bf3f10d0896841
HassanSurmount67463 <hassan67463@gmail.com>
2024-06-25 14:22:26 +0500

    update model nft

--- api/admin.py
+++ api/admin.py
@@ -4,4 +4,4 @@ from .models import NFT
 
 @admin.register(NFT)
 class NFTAdmin(admin.ModelAdmin):
-    list_display = ["id", "name", "price", "created_at", "updated_at"]
+    list_display = ["id", "name", "price", "image", "created_at", "updated_at"]
--- api/models.py
+++ api/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class NFT(models.Model):
     name = models.CharField(max_length=100)
     price = models.IntegerField()
+    image = models.ImageField(upload_to="nft/images")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
